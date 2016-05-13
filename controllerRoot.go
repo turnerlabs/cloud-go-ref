0648297f2328c839a68dd87d7db1683204fb8a07
Mickey Yawn <mickeyyawn@gmail.com>
2016-05-13 11:29:01 -0400

    fixed gcloud project name

--- controllerRoot.go
+++ controllerRoot.go
@@ -9,3 +9,21 @@ func (rc Controller) root(w http.ResponseWriter, r *http.Request) {
 	log.WithFields(logrus.Fields{"area": "rootController"}).Info("Root controller.")
 	w.Write([]byte("Hello from cloud-go-ref"))
 }
+
+
+
+func render() string {
+
+  html := `<!doctype html>
+           <head>
+             <title>hello from golang!</title>
+           </head>
+           <body>
+           <h1>Hello World!  This is cloud-go-ref...</h1>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+           </body>
+           </html>`
+
+  return html
+
+}
